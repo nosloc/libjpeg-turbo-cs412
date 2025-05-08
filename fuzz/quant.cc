2e502574011bcfc9335b70321ac2c34df3461e25
Myriam-Himmi <myriam.himmi.03@gmail.com>
2025-05-08 19:20:41 +0200

    testing build

--- fuzz/quant.cc
+++ fuzz/quant.cc
@@ -14,7 +14,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
     // Make a writable copy
     uint8_t *mutable_data = (uint8_t *)malloc(size*8);
     if (!mutable_data) return 0;
-    memcpy(mutable_data, data, size);
+    //memcpy(mutable_data, data, size);
 
     tjhandle handle = NULL;
     unsigned char *dstBuf = NULL; unsigned char *yuvBuf = NULL;
